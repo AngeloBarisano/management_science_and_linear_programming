105810f52ffb6deda935ac1fc1175ea1c03643ae
Angelo Barisano <barisano@web.de>
2022-10-22 22:46:12 +0200

    initial commit

---
+++ management_science_and_linear_programming/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is a course for university set up in the geneal venv form',
+    author='Angelo Barisano',
+    license='',
+)
